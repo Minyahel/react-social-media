8f9cbf9eaea5e33d721c928d35d44147c5724fa5
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2021-03-31 11:57:16 +0200

    mongoose connected, user model created

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { ApolloServer } = require(('apollo-server'));
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
     type Query {
-        sayHi: String!
+        sayHi: String!, 
+        test: String!
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        sayHi: () => 'Hello World!',
+        test: () => 'Testing'
     }
 }
 
@@ -18,6 +23,12 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.listen({ port: 5000}).then(res => {
-    console.log(`Server running at ${res.url}`)
-})
+mongoose.connect(MONGODB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then (() => {
+        console.log("Connected Successfuly");
+        return server.listen({ port: 5000})
+    })
+    .then ((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+
