dfc4f0403e07f93976fffacdf5458447ed6fcf7e
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2021-04-01 10:23:41 +0200

    Implemented user login and validation. Implemented token generation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -24,7 +24,8 @@ module.exports = gql`
         getPosts: [Post] 
     }
     type Mutation {
-        register(registerInput: RegisterInput): User
+        register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!): User!
     }
 `;
 
--- util/validators.js
+++ util/validators.js
@@ -27,5 +27,19 @@ module.exports.validateRegisterInput = (
         errors,
         valid: Object.keys(errors).length < 1
     }
-   
 }
+
+module.exports.validateLoginInput = (username, password) => {
+    const errors = {};
+    if (username.trim() === '') {
+        errors.username = 'Username must not be empty';
+    }
+    if (password.trim() === '') {
+        errors.password = 'Password must not be empty';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
+
