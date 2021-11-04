667dbc519b1ba6bf21afd1a3a90bcec5739c8d47
Minyahel Fekadu Haile <minyahelfekadu13@gmail.com>
2021-11-04 10:08:15 +0100

    finalising

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -3,42 +3,69 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 
 import { Button, Icon, Confirm } from "semantic-ui-react";
-import { FETCH_POSTS_QUERY } from '../util/graphql';
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
+function DeleteButton({ postId, commentId, callback }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+  const [deletePostOrMutation] = useMutation(mutation, {
+    update(proxy) {
+      setConfirmOpen(false);
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
+        data.getPosts = data.getPosts.filter((p) => p.id !== postId);
+        proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      }
+      if (callback) callback();
+    },
+    variables: {
+      postId,
+      commentId,
+    },
+  });
+
+  return (
+    <>
+      <Button
+        as="div"
+        color="red"
+        floated="right"
+        onClick={() => setConfirmOpen(true)}
+      >
+        <Icon name="trash" style={{ margin: 0 }} />
+      </Button>
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePostOrMutation}
+      />
+    </>
+  );
+}
 
 const DELETE_POST_MUTATION = gql`
-    mutation deletePost($postId: ID!) {
-        deletePost(postId: $postId)
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+
+      commentCount
     }
-`
-
-
-
-function DeleteButton( { postId, callback } ) {
-    const [confirmOpen, setConfirmOpen] = useState(false);
-
-    const [deletePost] = useMutation(DELETE_POST_MUTATION, {
-        update(proxy) {
-            setConfirmOpen(false);
-            const data = proxy.readQuery({
-                query: FETCH_POSTS_QUERY
-            });
-            data.getPosts = data.getPosts.filter(p => p.id !== postId);
-            if (callback) callback();
-        },
-        variables: {
-            postId
-        }
-    })
-
-    return (
-        <>
-        <Button as="div" color="red" floated="right" onClick={() => setConfirmOpen(true)}>
-            <Icon name="trash" style={{margin:0}}/>
-        </Button>
-        <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePost}/>
-        </>
-    )
-}
+  }
+`;
 
 export default DeleteButton;
--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,98 +1,144 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
-import { useQuery } from '@apollo/react-hooks';
-import { Button, Card, CardContent, Grid, Label, Image, Icon } from "semantic-ui-react";
+import { useQuery } from "@apollo/react-hooks";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Label,
+  Image,
+  Icon,
+} from "semantic-ui-react";
 import moment from "moment";
 import LikeButton from "../components/LikeButton";
 
 import { AuthContext } from "../context/auth";
 import DeleteButton from "../components/DeleteButton";
 
-
 function SinglePost(props) {
-    const postId = props.match.params.postId;
+  const postId = props.match.params.postId;
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    const { data } = useQuery(FETCH_POST_QUERY, {
-        variables: {
-            postId
-        }
-    })
+  const { data } = useQuery(FETCH_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
 
-    function deletePostCallback() {
-        props.history.push('/');
-    }
+  function deletePostCallback() {
+    props.history.push("/");
+  }
 
-    let postMarkup;
+  let postMarkup;
 
-    if (!data) {
-        postMarkup = <p>Loading post...</p>
-    } else {
-        const {id, body, createdAt, username, comments, likes, likeCount, commentCount} = data.getPost;
+  if (!data) {
+    postMarkup = <p>Loading post...</p>;
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      comments,
+      likes,
+      likeCount,
+      commentCount,
+    } = data.getPost;
 
-        postMarkup = (
-            <Grid>
-                <Grid.Row>
-                <Grid.Column width={2}>
-                    <Image
-                    floated='right'
-                    size='mini'
-                    src='https://react.semantic-ui.com/images/avatar/large/molly.png'
-                    />
-                </Grid.Column>
-                <Grid.Column width={10}>
-                <Card fluid>
-                    <Card.Content>
-                        <Card.Header>{username}</Card.Header>
-                        <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
-                        <Card.Description>{body}</Card.Description>
-                    </Card.Content>
-                    <hr/>
-                    <CardContent extra>
-                        <LikeButton user={user} post={{id, likeCount, likes}} />
-                        <Button as="div" labelPosition="right" onClick={() => console.log('Comment')}>
-                            <Button basic color="blue">
-                                <Icon name="comments"/>
-                            </Button>
-                            <Label basic color="blue" pointing="left">
-                                {commentCount}
-                            </Label>
-                        </Button>
-                        {user && user.username === username && (
-                            <DeleteButton postId={id} callback={deletePostCallback}/>
-                        )}
-                    </CardContent>
-                </Card>
-                </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        )
-    }
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              floated="right"
+              size="mini"
+              src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <CardContent extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log("Comment")}
+                >
+                  <Button basic color="blue">
+                    <Icon name="comments" />
+                  </Button>
+                  <Label basic color="blue" pointing="left">
+                    {commentCount}
+                  </Label>
+                </Button>
+                {user && user.username === username && (
+                  <DeleteButton postId={id} callback={deletePostCallback} />
+                )}
+              </CardContent>
+            </Card>
+            {comments.map((comment) => {
+              <Card fluid key={comment.id}>
+                <Card.Content>
+                  {user && user.username === comment.username && (
+                    <DeleteButton postId={id} commentId={comment.id} />
+                  )}
+                  <Card.Header>{comment.username}</Card.Header>
+                  <Card.Meta>{moment(comment.createdAt).fromNow()}</Card.Meta>
+                  <Card.Description>{body}</Card.Description>
+                </Card.Content>
+              </Card>;
+            })}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 
-    return postMarkup;
+  return postMarkup;
 }
 
-const FETCH_POST_QUERY = gql `
-    query($postId: ID!) {
-            getPost(postId: $postId) {
-            id
-            body 
-            createdAt
-            username
-            likeCount
-            likes {
-                username
-            }
-            commentCount
-            comments {
-                id
-                username
-                createdAt
-                body
-            }
-        }
+const FETCH_POST_QUERY = gql`
+  query ($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
     }
+  }
 `;
 
+const SUBMIT_COMMENT_MUTATION = gql`
+  mutation ($postId: ID!, $body: String!) {
+    createComment(postId: $postId, body: $body) {
+      id
+      comments {
+        id
+        body
+        createdAt
+        username
+      }
+      commentCount
+    }
+  }
+`;
 export default SinglePost;
